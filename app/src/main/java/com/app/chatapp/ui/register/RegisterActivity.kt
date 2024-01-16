96647bc7f8aec097e82f22c5ed3736a4eaa1e1db
Ngoc Hưng Lê <ngochungle1331@gmail.com>
2024-01-16 23:25:51 +0700

    login, sign up setup

---
+++ app/src/main/java/com/app/chatapp/ui/register/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package com.app.chatapp.ui.register
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.chatapp.R
+import com.app.chatapp.databinding.ActivityRegisterBinding
+import com.app.chatapp.ui.login.LoginActivity
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupEvents()
+    }
+
+    private fun setupEvents() {
+        binding.tvGoToLogin.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
